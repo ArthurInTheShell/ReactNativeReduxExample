5cded6157b13c0ab39e663844d5f1dda6fbbc336
Junhao Chen <junhaochenarthur@outlook.com>
2018-08-03 15:13:46 -0400

    get Redux to work

--- src/InnerComponent.js
+++ src/InnerComponent.js
@@ -2,31 +2,47 @@ import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import { store } from './store';
 import {types} from './actions';
+import {connect} from 'react-redux';
 // Now we can dispatch actions, which are understood by our store/reducer
 store.dispatch({ type: types.INCREMENT })
 store.dispatch({ type: types.INCREMENT })
 store.dispatch({ type: types.DECREMENT })
 store.dispatch({ type: types.DECREMENT })
 
-export default class InnerComponent extends React.Component {
+class InnerComponent extends React.Component {
     render() {
         return (
             <View
                 style={{ marginTop: 100 }}
             >
                 <Text style={{ fontSize: 100 }}>
-                    {store.getState().count}
+                    {this.props.count}
                 </Text>
 
-                {/* <Button
+                <Button
                 title='Click to increment'
-                onPress={() => store.dispatch({ type: types.INCREMENT })} />
+                onPress={() => this.props.add()} />
 
             <Button
                 title='Click to decrement'
-                onPress={() => store.dispatch({ type: types.DECREMENT })} /> */}
+                onPress={() => this.props.sub()} />
 
             </View>
         )
     }
 }
+
+const mapStateToProps = (state) =>{
+    return {
+        count: state.count
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+    return{
+        add:()=> dispatch({ type: types.INCREMENT }),
+        sub:()=> dispatch({ type: types.DECREMENT })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(InnerComponent)
